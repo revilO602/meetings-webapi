6faf9a5d5a6700656d8b7e93fd38868245a50547
Oliver Leontiev <leontiev.oliver@gmail.com>
2024-05-26 12:25:10 +0200

    Added dockerization

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -36,6 +36,9 @@ switch ($command) {
     "test" {
         go test -v ./...
     }
+    "docker" {
+       docker build -t revilo602/meetings-webapi:local-build -f ${ProjectRoot}/build/docker/Dockerfile .
+   }
     default {
         throw "Unknown command: $command"
     }
